ebd8e87e758c755ebe77ec73a81c11b3d65ebf59
Hasan Abbas <123466612+2022204@users.noreply.github.com>
2024-03-05 15:14:35 +0500

    python classes

--- pythonbyShifu!!/firstpython.py
+++ pythonbyShifu!!/firstpython.py
@@ -1,3 +1,8 @@
-import re
+import csv
 
-email = input("EMail: ")
+with open("pythonbyShifu!!/marsk.csv", "r") as file:
+    csv_reader = csv.DictReader(file)
+    for line in csv_reader:
+        name = line["names"]
+        marks = line["marks"]
+        print(f"Name: {name}   Marks:{marks}")
