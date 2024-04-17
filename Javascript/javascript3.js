ffb9120557e8dbe47adf1806a76071c5846ccbb2
Hasan Abbas <123466612+2022204@users.noreply.github.com>
2024-04-17 14:37:00 +0500

    java

---
+++ Javascript/javascript3.js
@@ -0,0 +1,42 @@
+// for (let i = 1; i <=5; i++){
+//     console.log("apna college");
+// }
+
+
+
+// let i = 0;
+// while(i < = 4){
+//     console.log("Apna College");
+//     i++;
+// }
+
+
+
+// let j = 0;
+// do{
+//    console.log("apna college");
+// }while(j < 2);
+
+
+
+//for-of loop
+
+// let str = "ApnaCollege";
+// for(let i of str){
+//     console.log(i);
+// }
+
+
+//for in loop
+
+let student = {
+    name: "hasan",
+    age: 23,
+    cgpa: 7.5,
+    isPass:true
+};
+
+for(let i in student){
+    console.log(student[i]);
+}
+
