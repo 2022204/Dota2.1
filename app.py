98789923aa6ba287ba246eb7d6c18ca8ce399882
Hasan Abbas <123466612+2022204@users.noreply.github.com>
2024-04-24 19:23:40 +0500

    updated

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ users = [{"username":"hasan","password":"hasan", "value":123},
 
 duels = [
         {'username':'hasan','opponent': 'Murtaza', 'time': '12:00', 'result': 'Won', 'exchange':100},
-        {'username':'hasan','opponent': 'Farza', 'time': '12:30', 'result': 'Lost', 'exchange':-230},
+        {'username':'hasan','opponent': 'farza', 'time': '12:30', 'result': 'Lost', 'exchange':-230},
         {'username':'hasan','opponent': 'Maryam', 'time': '13:00', 'result': 'Won', 'exchange': 102},
         {'username':'farza','opponent': 'Hasan', 'time': '12:30', 'result': 'Won', 'exchange': 230},
     ]
