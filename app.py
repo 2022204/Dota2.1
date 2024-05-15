21599e5eb71a92a6ff574c16420ce3d3fe7c1901
Hasan Abbas <u2022204@giki.edu.pk>
2024-05-15 09:32:00 +0500

    asd

--- app.py
+++ app.py
@@ -420,13 +420,6 @@ def fight():
         )
 
 
-# @app.after_request
-# def after_request(response):
-#     """Ensure responses aren't cached"""
-#     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-#     response.headers["Expires"] = 0
-#     response.headers["Pragma"] = "no-cache"
-#     return response
 
 
 @app.route("/fighting", methods=["POST"])
